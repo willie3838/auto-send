7b7fb1383142a32be483655b83c471ecf928f63b
William Wai Lim Chan <wchan365@uwo.ca>
2021-06-24 19:16:07 -0400

    added captcha

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -55,8 +55,8 @@ const Home = () => {
         <DialogTitle>Error</DialogTitle>
         <DialogContent>
           <Typography gutterBottom>
-            If you have not allowed access to use less secure apps by clicking the link under password. 
-            Otherwise, you input an incorrect email or password, please try again.
+            To ensure your login in succeeds, make sure you click both links under password to enable this 
+            app to access your account. Otherwise, you input an incorrect email or password, please try again.
           </Typography>
         </DialogContent>
         <DialogActions>
@@ -91,12 +91,19 @@ const Home = () => {
               fullWidth={true}
             />
             <div>
-              <div class="display-inline mt-1">
-                <Link href="https://myaccount.google.com/lesssecureapps?pli=1&rapt=AEjHL4MIyWKmQ1ug7ushrtZw2yrOZlFb1dJpsRFiPQGG_W1gjS8piN8qOxuKwJ8qObblHI3UfsVSQ9hPjxX27hFq9zHbM64Lfg" target="_blank" rel="noopener">Turn on Less Secure Apps</Link>
+              <div class="mt-2">
+                
+           
+                <Link href="https://myaccount.google.com/lesssecureapps?pli=1&rapt=AEjHL4MIyWKmQ1ug7ushrtZw2yrOZlFb1dJpsRFiPQGG_W1gjS8piN8qOxuKwJ8qObblHI3UfsVSQ9hPjxX27hFq9zHbM64Lfg" display="block" target="_blank" rel="noopener">Turn on Less Secure Apps</Link>
+    
+                <Link href="https://accounts.google.com/b/0/DisplayUnlockCaptcha" display="block" target="_blank" rel="noopener">Unlock Captcha</Link>
+            
+               
+  
               </div>
             </div>
           </div>
-          <div class="w-96 text-right">
+          <div class="w-96 relative text-right bottom-10">
             <LoginButton onClick={handleClick.bind()} variant="contained">
               Login
             </LoginButton>
