e011fc64897736eff42554df5457061f0733d247
William Wai Lim Chan <wchan365@uwo.ca>
2021-06-24 18:02:56 -0400

    changed routes

--- dist/index.js
+++ dist/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "../client/build")));
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "../client/build", "index.html"));
 });
 
